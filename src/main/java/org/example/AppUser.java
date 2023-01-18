24d7bb764ced159ab6523ec831a8163c04bc29c2
Annika <a.staalhandske@gmail.com>
2023-01-18 15:22:40 +0100

    appUserTest-red-state

---
+++ src/main/java/org/example/AppUser.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Objects;
+
+public class AppUser {
+
+    private String username;
+    private String password;
+
+    public AppUser(String username, String password) {
+        this.username=username;
+        this.password=password;
+    }
+
+    public String getUsername() {
+        return "";
+    }
+
+    public String getPassword() {
+        return "";
+    }
+
+
+}
---
+++ src/test/java/org/example/AppUserTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppUserTest {
+
+
+    @Test
+    public void compareUsername_sameUsername_returnUsername(){
+        //Given
+        AppUser appUser;
+
+        //When
+        appUser=new AppUser("CrazyDoris", "1234");
+        String username = appUser.getUsername();
+        System.out.println(appUser.getUsername());
+        String password = appUser.getPassword();
+
+
+        //Then
+        assertEquals("CrazyDoris", username);
+    }
+
+}
