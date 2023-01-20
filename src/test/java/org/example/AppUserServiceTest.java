bb9d34290fc246b5db3e04314c594c2081618460
Annika <a.staalhandske@gmail.com>
2023-01-20 10:46:02 +0100

    new test for 100coverage-red-state

--- src/test/java/org/example/AppUserServiceTest.java
+++ src/test/java/org/example/AppUserServiceTest.java
@@ -2,10 +2,13 @@ package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppUserServiceTest {
+
     private AppUser appUser;
 
     @BeforeEach()
--- src/test/java/org/example/AppUserTest.java
+++ src/test/java/org/example/AppUserTest.java
@@ -1,22 +1,38 @@
 package org.example;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 public class AppUserTest {
+private AppUser appUser;
 
+@BeforeEach
+public void startUp(){
+    appUser=new AppUser("Ankan", "1234");
+}
 
     @Test
     public void compareUsername_sameUsername_returnUsername(){
         //Given
-        AppUser appUser;
+//        appUser=new AppUser("Ankan", "1234");
 
         //When
-        appUser=new AppUser("CrazyDoris", "1234");
         String username = appUser.getUsername();
 
         //Then
-        assertEquals("CrazyDoris", username);
+        assertEquals("Ankan", username);
+    }
+
+    //test för att gämföra två användare,
+    @Test
+    public void compareUserData_ifSameUser_returnUser(){
+        //Given
+
+        //When
+        String username=null;
+        String password=null;
+
+        //Then
+        assertAll(()->assertNotNull(appUser), ()->assertEquals("Annika",username),()->assertEquals("12345",password));
     }
 
 }
